d0afff7fecd377c5d55e8d30dbd1f86eb851b8ef
gabriel <gabrielmouraguimaraes@gmail.com>
2022-09-28 23:24:18 -0300

    resolvendo a questão 3

---
+++ trabalho1/questao3/q3.py
@@ -0,0 +1,12 @@
+# Desenvolva um Programa em PYTHON usando o comando FOR, 
+# para imprimir os números pares de 0 a 100 e a soma destes números.
+
+numero = 0;
+soma = 0;
+
+for numero in range(0, 100, 1):
+    if numero % 2 == 0:            
+        print("Valor: ", numero)
+        res = soma + numero
+        print("Soma: ", soma, " + ", numero, " = ", res, "\n-----------------------------")
+        soma = res
