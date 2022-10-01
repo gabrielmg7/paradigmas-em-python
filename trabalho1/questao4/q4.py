a1e7c39c040651140725a89eeca3e22247cc5077
gabriel <gabrielmouraguimaraes@gmail.com>
2022-10-01 14:35:29 -0300

    resolvendo a questão 4

---
+++ trabalho1/questao4/q4.py
@@ -0,0 +1,13 @@
+# Desenvolva um Programa em PYTHON usando o comando FOR, 
+# para calcular e imprimir o fatorial de N, informado pelo usuário.
+
+print("Calculo do Fatorial.")
+print("\n")
+num = int(input("Insira o valor a ser calculado: "))
+
+res = 1 
+count = 1
+
+for n in range(1, num + 1):
+    res *= n
+print(res)
